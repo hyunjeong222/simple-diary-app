116b0f365ae2c47271dc18712967fd69a9489486
hyunjeong222 <hyenj629@naver.com>
2022-12-29 23:53:56 +0900

    update

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -5,19 +5,17 @@ const DiaryEditor = ()=>{
         author: "",
         content: "",
     });
+    const handleChangeState = (e)=>{
+        console.log(e.target.name);
+        console.log(e.target.name);
+    }
     return <div className="DiaryEditor">
         <h2>오늘의 일기</h2>
         <div>
             <input 
                 name = "author"
                 value={state.author} 
-                onChange={(e) => {
-                    setState({
-                        ...state, 
-                        // 원래 있던 state를 펼쳐주고 나서 변경하고자 하는 state의 property 적어준다. -> 순서가 바뀌면 업데이트된 값에 원래 값이 덮어씌워짐
-                        author: e.target.value,
-                    });
-                }}
+                onChange={handleChangeState}
             />
         </div>
         <div>
