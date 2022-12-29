db3935c66a187ea7a14fc2f53156d9aa8faf7f02
hyunjeong222 <hyenj629@naver.com>
2022-12-29 23:50:50 +0900

    1229 update

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -13,7 +13,8 @@ const DiaryEditor = ()=>{
                 value={state.author} 
                 onChange={(e) => {
                     setState({
-                        ...state, // 원래 있던 state를 펼쳐주고 나서 변경하고자 하는 state의 property 적어준다. -> 순서가 바뀌면 업데이트된 값에 원래 값이 덮어씌워짐
+                        ...state, 
+                        // 원래 있던 state를 펼쳐주고 나서 변경하고자 하는 state의 property 적어준다. -> 순서가 바뀌면 업데이트된 값에 원래 값이 덮어씌워짐
                         author: e.target.value,
                     });
                 }}
