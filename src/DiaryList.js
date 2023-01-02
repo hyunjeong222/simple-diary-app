41f054932b4a2e4c40047eb5c849457301fc0d24
hyunjeong222 <hyenj629@naver.com>
2023-01-02 20:14:56 +0900

    0102 react 데이터 수정

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,15 +1,16 @@
 import DiaryItem from "./DiaryItem";
 
 // react는 단방향으로 데이터가 흐름
+// 데이터는 위에서 아래로, 이벤트는 아래에서 위로
 
-const DiaryList = ({ onDelete, diaryList })=>{
+const DiaryList = ({ onEdit, onRemove, diaryList })=>{
     return (
         <div className="DiaryList">
             <h2>일기 리스트</h2>
             <h4>{diaryList.length}개의 일기가 있습니다.</h4>
             <div>
                 {diaryList.map((it) => (
-                    <DiaryItem key={it.id} {...it} onDelete={onDelete} />
+                    <DiaryItem key={it.id} {...it} onEdit={onEdit} onRemove={onRemove} />
                 ))}
             </div>
         </div>
