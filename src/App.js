943aa7d3c28348b8d9b74cdd4058402333345296
hyunjeong222 <hyenj629@naver.com>
2023-01-03 21:47:29 +0900

    0103 react useCallback

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useMemo, useEffect, useRef, useState } from 'react';
+import { useEffect, useCallback, useMemo, useRef, useState } from 'react';
 import './App.css';
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from './DiaryList';
@@ -30,7 +30,7 @@ function App() {
     getData();
   }, [])
 
-  const onCreate = (author, content, emotion)=>{
+  const onCreate = useCallback((author, content, emotion)=>{
     const created_date = new Date().getTime();
     const newItem = {
       author,
@@ -40,8 +40,8 @@ function App() {
       id: dataId.current
     }
     dataId.current += 1;
-    setData([newItem, ...data]);
-  };
+    setData((data)=>[newItem, ...data]);
+  }, []);
 
   const onRemove = (targetId) => {
     const newDiaryList = data.filter((it) => it.id !== targetId);
